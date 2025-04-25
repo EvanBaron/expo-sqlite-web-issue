d17f6f621a58e7364552f5de27a62d5ecdd42e1d
Evan Baron <evanbaron.a4@gmail.com>
2025-04-25 11:29:43 +0200

    minimal reproducible example

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import AsyncStorage from "expo-sqlite/kv-store";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [language, setLanguage] = useState("en");
+
+  useEffect(() => {
+    const loadLanguage = async () => {
+      const savedLanguage = await AsyncStorage.getItem("language");
+      console.log("Saved language:", savedLanguage);
+      setLanguage(savedLanguage || "en");
+    };
+
+    loadLanguage();
+  }, []);
+
+  const changeLanguage = async (lang: string) => {
+    await AsyncStorage.setItem("language", lang);
+    setLanguage(lang);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Text>{language}</Text>
       <StatusBar style="auto" />
+      <View>
+        {Object.entries({
+          en: "English",
+          fr: "French",
+          es: "Spanish",
+        }).map(([code, lang]) => (
+          <Pressable key={code} onPress={() => changeLanguage(code)}>
+            <Text>{lang}</Text>
+          </Pressable>
+        ))}
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
